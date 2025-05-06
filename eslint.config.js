100b42171643930bf053d1b70b399bf2c5529837
NaeemAfridi <coder331@gmail.com>
2025-05-06 15:03:50 +0500

    build commands added

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+// eslint.config.js
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+    },
+  },
+];
