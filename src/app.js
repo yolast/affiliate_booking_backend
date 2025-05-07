b24d81ef012c60b62607f71b2d2705374a28e26e
NaeemAfridi <coder331@gmail.com>
2025-05-07 11:01:17 +0500

    set CORS manually

--- src/app.js
+++ src/app.js
@@ -9,11 +9,6 @@ import compression from "compression";
 import { errorHandler } from "./utils/error.utils.js";
 import logger from "./config/logger.js";
 import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-//=========== configure dotenv
-dotenv.config({
-  path: "./.env",
-});
 
 // Import routes
 // import authRoutes from "./routes/auth.routes.js";
@@ -29,17 +24,39 @@ import affiliateRoutes from "./routes/affiliate.routes.js";
 const app = express();
 
 // ======== CORS
-const corsOptions = {
-  origin: process.env.CORS_ORIGIN,
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
+// Set middleware of CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://www.yolast.com");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
 
-app.use(cors(corsOptions));
+  next();
+});
 
-// Handle preflight requests
-app.options("*", cors(corsOptions));
+// Set preflight
+app.options("*", (req, res) => {
+  console.log("preflight");
+  if (
+    req.headers.origin === "https://affiliate-booking-backend.onrender.com" &&
+    allowMethods.includes(req.headers["access-control-request-method"]) &&
+    allowHeaders.includes(req.headers["access-control-request-headers"])
+  ) {
+    console.log("pass");
+    return res.status(204).send();
+  } else {
+    console.log("fail");
+  }
+});
 
 // Set security HTTP headers
 app.use(helmet());
