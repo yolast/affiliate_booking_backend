59d7f8d0a69630e791cb1f65dae357d5781950e2
NaeemAfridi <coder331@gmail.com>
2025-05-07 11:23:03 +0500

    cors setup cleaned

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ import compression from "compression";
 import { errorHandler } from "./utils/error.utils.js";
 import logger from "./config/logger.js";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+//=========== configure dotenv
+dotenv.config({
+  path: "./.env",
+});
 
 // Import routes
 // import authRoutes from "./routes/auth.routes.js";
@@ -24,39 +29,12 @@ import affiliateRoutes from "./routes/affiliate.routes.js";
 const app = express();
 
 // ======== CORS
-// Set middleware of CORS
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://www.yolast.com");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
-
-// Set preflight
-app.options("*", (req, res) => {
-  console.log("preflight");
-  if (
-    req.headers.origin === "https://affiliate-booking-backend.onrender.com" &&
-    allowMethods.includes(req.headers["access-control-request-method"]) &&
-    allowHeaders.includes(req.headers["access-control-request-headers"])
-  ) {
-    console.log("pass");
-    return res.status(204).send();
-  } else {
-    console.log("fail");
-  }
-});
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    Credentials: true,
+  })
+);
 
 // Set security HTTP headers
 app.use(helmet());
